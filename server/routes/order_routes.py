28c2d641317cc75dc8ee6509d32d5cd881d6b2bc
Verah Mokaya <verahmokaya475@gmail.com>
2026-01-13 21:21:05 +0300

    added orders class to filter by customers id and confirm order status

--- server/routes/order_routes.py
+++ server/routes/order_routes.py
@@ -1,18 +1,18 @@
-from flask import request
+from flask import request, jsonify
 from flask_restful import Resource
-from models import db, Order, customers
+from models import db, Customer, Order
 
 class Orders(Resource):
     def get(self):
-        orders = Order.query.all()
-        return [order.to_dict() for order in orders]
+        customer_id = request.args.get('customer_id')
+        status = request.args.get('status')
 
-    def post(self):
-        data = request.get_json()
-        customer_id = data.get('customer_id')
-        order_date = data.get('order_date')
+        query = Order.query
 
-        new_order = Order(customer_id=customer_id, order_date=order_date)
-        db.session.add(new_order)
-        db.session.commit()
-        return new_order.to_dict(), 201
+        if customer_id:
+            query = query.filter_by(customer_id=customer_id)
+        if status:
+            query = query.filter_by(status=status)
+        return jsonify([order.to_dict() for order in query.all()])
+    
+    
